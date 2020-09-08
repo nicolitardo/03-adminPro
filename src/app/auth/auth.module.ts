b35d162eb6cf855288d37936477908fd5ccc9d74
Nico <pablolitardo@gmail.com>
2020-09-08 19:26:40 -0300

    Fin secciòn 4 - Router

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RouterModule } from '@angular/router';
+
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+  ],
+  exports: [
+    LoginComponent,
+    RegisterComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class AuthModule { }
